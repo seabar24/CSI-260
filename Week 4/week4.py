4aabfe28fd0202b57f1614d1fc5bd3ceb987eeac
Sean Barrick <71162295+seabar24@users.noreply.github.com>
2024-02-19 22:47:11 -0500

    Update week4.py

--- Week 4/week4.py
+++ Week 4/week4.py
@@ -1,3 +1,21 @@
+"""DESCRIPTION OF THE MODULE GOES HERE
+
+Author: Sean Barrick
+Class: CSI-260-01
+Assignment: Week 4 Lab
+Due Date: February 9, 2020 11:59 PM
+
+Certification of Authenticity:
+I certify that this is entirely my own work, except where I have given
+fully-documented references to the work of others. I understand the definition
+and consequences of plagiarism and acknowledge that the assessor of this
+assignment may, for the purpose of assessing this assignment:
+- Reproduce this assignment and provide a copy to another member of academic
+- staff; and/or Communicate a copy of this assignment to a plagiarism checking
+- service (which may then retain a copy of this assignment on its database for
+- the purpose of future plagiarism checking)
+"""
+
 from medical import Patient, Procedure
 import time
 import os
