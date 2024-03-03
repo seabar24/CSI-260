6add046a9600c0681bd8d7c3da1f384deb230d98
Sean Barrick <71162295+seabar24@users.noreply.github.com>
2024-03-03 18:45:14 -0500

    Create main.py

---
+++ Week 7/Case Study/main.py
@@ -0,0 +1,17 @@
+"""
+This code is only meant to test auth.py 
+Modify to test your code to make sure it complies
+with the assignment
+"""
+import auth
+
+try:
+    auth.authenticator.add_user("joe", "aaawwwww")
+    print ("you are ok")
+except auth.PasswordTooShort:
+    print('password is too short')
+except auth.PasswordMissingNumber:
+    print("password missing a number")
+except auth.PasswordMissingSC:
+    print("password missing a special character")
+
